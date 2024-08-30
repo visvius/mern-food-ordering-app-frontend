3685d1804cd726a0ea25ccea426e386e549c4984
visvius <vishal.singhofficial1@gmail.com>
2024-08-30 12:29:55 +0530

    removed unused variables

--- src/auth/Auth0ProviderWithNavigate.tsx
+++ src/auth/Auth0ProviderWithNavigate.tsx
@@ -1,5 +1,4 @@
-import { useCreateMyUser } from "@/api/MyUserApi";
-import { AppState, Auth0Provider, User } from "@auth0/auth0-react";
+import {  Auth0Provider } from "@auth0/auth0-react";
 import { useNavigate } from "react-router-dom";
 
 type Props = {
@@ -17,7 +16,7 @@ const Auth0ProviderWithNavigate = ({children}: Props) => {
     }
 
     // gets called after user Logs In
-    const onRedirectCallback = (appState?:AppState, user?:User) =>{
+    const onRedirectCallback = () =>{
         navigate('/auth-callback');
     }
 
