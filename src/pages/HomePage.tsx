eac3c351506bbd0aea4cce80d3665a881416f3bf
visvius <vishal.singhofficial1@gmail.com>
2024-09-16 20:33:56 +0530

    added results page with filters

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,29 +1,49 @@
-import landingImage from '../assets/landing.png'
-import appDownloadImage from '../assets/appDownload.png'
+import landingImage from "../assets/landing.png";
+import appDownloadImage from "../assets/appDownload.png";
+import SearchBar, { SearchForm } from "@/components/SearchBar";
+import { useNavigate } from "react-router-dom";
 
 function HomePage() {
-  return (
-    <div className="flex flex-col gap-12">
-        <div className="bg-white rounded-lg shadow-md py-8 flex flex-col gap-5 text-center -mt-16">
-            <h1 className="text-5xl font-bold tracking-tight text-orange-600">
-                Tuck into a takeaway today
-            </h1>
-            <span className="text-xl">Food is just a click away!</span>
-        </div>
-        <div className="grid md:grid-cols-2 gap-5">
-            <img src={landingImage} alt="landing img" />
-            <div className='flex flex-col item-center justify-center gap-4 text-center'>
-                <span className='font-bold text-3xl tracking-tight'>
-                    Order takeaway even faster!
-                </span>
-                <span>
-                    Download MernEats App for faster ordering and personalised recommendations
-                </span>
-                <img src={appDownloadImage} className='w-full max-w-xs mx-auto h-auto"' alt="App Download Img"/>
+    const navigate = useNavigate();
+
+    const handleSearchSubmit = (searchFormValue: SearchForm) => {
+        navigate({
+            pathname: `/search/${searchFormValue.searchQuery}`,
+        });
+    };
+
+    return (
+        <div className="flex flex-col gap-12">
+            {/* adjust the px and mx values below */}
+            <div className="md:px-20 bg-white rounded-lg shadow-md py-8 flex flex-col gap-5 text-center -mt-16 md:mx-16">
+                <h1 className="text-5xl font-bold tracking-tight text-orange-600">
+                    Tuck into a takeaway today
+                </h1>
+                <span className="text-xl">Food is just a click away!</span>
+                <SearchBar
+                    placeHolder="Search by City or Town"
+                    onSubmit={handleSearchSubmit}
+                />
+            </div>
+            <div className="grid md:grid-cols-2 gap-5">
+                <img src={landingImage} alt="landing img" />
+                <div className="flex flex-col item-center justify-center gap-4 text-center">
+                    <span className="font-bold text-3xl tracking-tight">
+                        Order takeaway even faster!
+                    </span>
+                    <span>
+                        Download MernEats App for faster ordering and
+                        personalised recommendations
+                    </span>
+                    <img
+                        src={appDownloadImage}
+                        className='w-full max-w-xs mx-auto h-auto"'
+                        alt="App Download Img"
+                    />
+                </div>
             </div>
         </div>
-    </div>
-  )
+    );
 }
 
-export default HomePage
+export default HomePage;
---
+++ src/pages/SearchPage.tsx
@@ -0,0 +1,104 @@
+import { useSearchRestaurants } from "@/api/RestaurantApi";
+import CuisineFilter from "@/components/CuisineFilter";
+import PaginationSelector from "@/components/PaginationSelector";
+import SearchBar, { SearchForm } from "@/components/SearchBar";
+import SearchResultInfo from "@/components/SearchResultInfo";
+import SearchResultsCard from "@/components/SearchResultsCard";
+import { useState } from "react";
+import { useParams } from "react-router-dom";
+
+export type SearchState = {
+    searchQuery: string;
+    page: number;
+    selectedCuisines: string[];
+};
+
+const SearchPage = () => {
+    const { city } = useParams();
+    const [searchState, setSearchState] = useState<SearchState>({
+        searchQuery: "",
+        page: 1,
+        selectedCuisines: [],
+    });
+
+    const [isExpanded, setIsExpanded] = useState<boolean>(false);
+
+    const { results, isLoading } = useSearchRestaurants(searchState, city);
+
+    const setSelectedCuisines = (selectedCuisines: string[]) => {
+        setSearchState((prevState) => ({
+            ...prevState,
+            selectedCuisines,
+            page: 1,
+        }));
+    };
+
+    const setPage = (page: number) => {
+        setSearchState((prevState) => ({
+            ...prevState,
+            page,
+        }));
+    };
+
+    const setSearchQuery = (searchFormData: SearchForm) => {
+        setSearchState((prevState) => ({
+            ...prevState,
+            searchQuery: searchFormData.searchQuery,
+            page: 1,
+        }));
+    };
+
+    const resetSearch = () => {
+        setSearchState((prevState) => ({
+            ...prevState,
+            searchQuery: "",
+            page: 1,
+        }));
+    };
+
+    if (isLoading) {
+        return <span>Loading...</span>;
+    }
+
+    if (!results?.data || !city) {
+        return <span>No results found</span>;
+      }
+
+    return (
+        <div className="grid grid-cols-1 lg:grid-cols-[250px_1fr] gap-5">
+            <div id="cuisines-list">
+                <CuisineFilter
+                    selectedCuisines={searchState.selectedCuisines}
+                    onChange={setSelectedCuisines}
+                    isExpanded={isExpanded}
+                    onExpandedClick={() => setIsExpanded((prev) => !prev)}
+                />
+            </div>
+            <div id="main-content" className="flex flex-col gap-5">
+                <SearchBar
+                    searchQuery={searchState.searchQuery}
+                    onSubmit={setSearchQuery}
+                    placeHolder="Search by Cuisine or Restaurant Name"
+                    onReset={resetSearch}
+                />
+                <SearchResultInfo
+                    total={results.pagination.total}
+                    city={city}
+                />
+                {results.data.map((restaurant) => (
+                    <SearchResultsCard
+                        key={restaurant._id}
+                        restaurant={restaurant}
+                    />
+                ))}
+                <PaginationSelector
+                    page={results.pagination.page}
+                    pages={results.pagination.pages}
+                    onPageChange={setPage}
+                />
+            </div>
+        </div>
+    );
+};
+
+export default SearchPage;
