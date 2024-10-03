309a2d05b3a31627428d92eb6fb452c79fe7f258
visvius <vishal.singhofficial1@gmail.com>
2024-10-03 12:31:50 +0530

    logout fix for small devices

--- src/components/MobileNavLinks.tsx
+++ src/components/MobileNavLinks.tsx
@@ -27,7 +27,13 @@ function MobileNavLinks() {
                 User Profile
             </Link>
             <Button
-                onClick={() => logout()}
+                onClick={() =>
+                    logout({
+                        logoutParams: {
+                            returnTo: `${window.location.origin}`,
+                        },
+                    })
+                }
                 className="flex items-center px-3 font-bold hover:bg-gray-500"
             >
                 Log Out
